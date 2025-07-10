e666388c85fa7c359c7ed5badda1602d035cb6a2
sodmaq <sodmaq@gmail.com>
2025-07-10 10:59:57 +0100

    db set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    UserModule,
     DatabaseModule,
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
