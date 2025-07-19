be6bc7ef18f9b39dd7a41fb4f0d3e298918b4dca
Yekini  Sodiq Opeyemi <78536302+sodmaq@users.noreply.github.com>
2025-07-19 18:00:44 +0100

    Delete .history directory

--- .history/src/user/decorator/get-user.decorator_20250719174206.ts
+++
@@ -1,7 +0,0 @@
-// get-user.decorator.ts
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
